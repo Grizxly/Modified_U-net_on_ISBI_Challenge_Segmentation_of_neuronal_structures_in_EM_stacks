7afa8cf0b5d5e5e43ee970a73da84f859a1a97e3
Grizxly <44309982+Grizxly@users.noreply.github.com>
2020-10-11 13:59:31 +0800

    Update run.py

--- run.py
+++ run.py
@@ -5,9 +5,9 @@ import cv2 as cv
 run unet to make predict on ONE image
 """
 
-IMG_PATH = '/home/xin/comp9517/proj/data/images/train-volume01.jpg'
-RESULT_PATH = '/home/xin/comp9517/proj/results'
-MODEL_PATH = '/home/xin/comp9517/proj/checkpoint/'
+IMG_PATH = './data/images/train-volume01.jpg'
+RESULT_PATH = './results'
+MODEL_PATH = './checkpoint/'
 MODEL_NAME = 'unet.ckpt'
 
 img_list = [cv.imread(IMG_PATH, 0)]
